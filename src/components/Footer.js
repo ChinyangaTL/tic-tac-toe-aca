4ec01fd40007f9b1f2c0ab08dde1bc81a8199b37
Les Chinyanga <les.chinyanga@africacode.academy>
2022-02-02 11:03:52 +0200

    Add missing Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as XGrey } from '../images/icons/icon-x-grey.svg';
+import { ReactComponent as OGrey } from '../images/icons/icon-o-grey.svg';
+
+const Footer = ({ currentPlayer }) => {
+  return (
+    <FooterWrapper>
+      <div className="turn-indicator">
+        {currentPlayer === 'x' ? <XGrey /> : <OGrey />} <p>Turn</p>
+      </div>
+    </FooterWrapper>
+  );
+};
+
+const FooterWrapper = styled.footer`
+  height: 3rem;
+  margin: 1rem 0;
+  .turn-indicator {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.5rem 0;
+    background-color: red;
+    border-radius: var(--radius);
+    background-color: var(--clr-btn-primary);
+    p {
+      margin-left: 1rem;
+      font-size: 1.4rem;
+      color: white;
+    }
+  }
+`;
+
+export default Footer;
